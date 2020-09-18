0da1a339f293ed6b5d132b537a6c8b953a69b2a3
zhaojingchao <2356430623@qq.com>
2020-09-18 08:11:46 +0800

    涉及模块：axios请求封装 详细描述：对home首页的网络请求进行封装

--- src/network/request.js
+++ src/network/request.js
@@ -3,37 +3,32 @@ import Axios from "axios";
 //使用Promise对象对网络请求进行封装
 export function request(config){
   // 创建Axios实例
-  const instance1 = Axios.create({
+  const instance = Axios.create({
     baseURL: "http://152.136.185.210:8000/api/z8",
     timeout: 5000
   })
 
-  // instance1实例的请求拦截器
-  instance1.interceptors.request.use(
+  // instance实例的请求拦截器
+  instance.interceptors.request.use(
     config => { // 请求成功时进行拦截
-      console.log("请求成功时进行拦截");
-      console.log(config);
       return config;  // 注意：在请求时进行拦截时，必须要返回拦截的config对象
     },
     error => { // 请求失败时进行拦截
-      console.log("请求失败时进行拦截");
       console.log(error);
     }
   )
 
   // 响应拦截
-  instance1.interceptors.response.use(
+  instance.interceptors.response.use(
     res => {
-      console.log("响应成功时进行拦截");
       return res.data  //在响应成功时，拦截器进行处理，只返回data
     },
     err => {
-      console.log("响应失败时进行拦截");
       console.log(err);
     }
   )
 
   // 发送真正的网络请求
-  return instance1(config)
+  return instance(config)
 }
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,12 +8,26 @@
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
+  import {getHomeMultiData} from "network/homeAPI";
 
   export default {
     name: "Home",
     components: {
       NavBar
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() { //使用created声明周期函数，组件一旦创建好后就发送网络请求
+      getHomeMultiData().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list
+      })
     }
+
   }
 </script>
 
